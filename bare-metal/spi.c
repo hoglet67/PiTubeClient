5fd7e15d6774933d353150c1aad6d121487a2a0b
David Banks <dave@hoglet.com>
2015-12-09 16:52:59 +0000

    Fixed a race condition in the SPI Driver
    Change-Id: I0e33a1a8356d19aac60a14a9fb5eb910afa709f0

--- bare-metal/spi.c
+++ bare-metal/spi.c
@@ -26,47 +26,43 @@ void spi_begin(void)
   spi_pin_init();
         
   SPI0_CONTROL = 0;
-  SPI0_CONTROL |= BIT(SPI_C_CLEAR_RX) | BIT(SPI_C_CLEAR_TX);                // Clear TX and RX fifos
+  SPI0_CONTROL |= BIT(SPI_C_CLEAR_RX) | BIT(SPI_C_CLEAR_TX);                 // Clear TX and RX fifos
     
-  SPI0_CONTROL &= ~(BIT(SPI_C_CPOL) | BIT(SPI_C_CPHA));                     // Set data mode = 0
-  SPI0_CONTROL &= ~(BIT(SPI_C_CS0) | BIT(SPI_C_CS1));                       // Set to CS0 and Chip Select Polarity=LOW
+  SPI0_CONTROL &= ~(BIT(SPI_C_CPOL) | BIT(SPI_C_CPHA));                      // Set data mode = 0
+  SPI0_CONTROL &= ~(BIT(SPI_C_CS0) | BIT(SPI_C_CS1));                        // Set to CS0 and Chip Select Polarity=LOW
   SPI0_CONTROL &= ~BIT(SPI_C_CSPOL0);
 
-  SPI0_CLK = 16;                                                                    // Set clock to ???
+  SPI0_CLK = 16;                                                             // Set clock to ???
 }
 
 void spi_transfer(u8* TxBuff, u8* RxBuff, u32 Length)
 {
-  SPI0_CONTROL |= BIT(SPI_C_CLEAR_RX) | BIT(SPI_C_CLEAR_TX) | BIT(SPI_C_TA);        // Clear TX and RX FIFOs and enable SPI
-
-  while (Length--)
+  u32 remaining;
+  SPI0_CONTROL |= BIT(SPI_C_CLEAR_RX) | BIT(SPI_C_CLEAR_TX) | BIT(SPI_C_TA); // Clear TX and RX FIFOs and enable SPI
+  remaining = Length;
+  while (remaining--)
     {
-      while ((SPI0_CONTROL & BIT(SPI_C_TXD)) == 0)                              // Wait for space in the FIFO
+      while ((SPI0_CONTROL & BIT(SPI_C_TXD)) == 0)                           // Wait for space in the FIFO
         {
           continue;
         }
 
-      SPI0_FIFO = *(TxBuff++);                                                      // Write to TX FIFO
-
-      while (SPI0_CONTROL & BIT(SPI_C_RXD))
-        {
-          *(RxBuff++) = SPI0_FIFO;                                                  // Read RX FIFO
-        }
+      SPI0_FIFO = *(TxBuff++);                                               // Write to TX FIFO
     }
-
-  while ((SPI0_CONTROL & BIT(SPI_C_DONE)) == 0)                             // While not done
+  remaining = Length;
+  while (remaining--)
     {
-      if (SPI0_CONTROL & BIT(SPI_C_RXD))
+      while ((SPI0_CONTROL & BIT(SPI_C_RXD)) == 0)                           // Wait for data in the FIFO
         {
-          *(RxBuff++) = SPI0_FIFO;                                                  // Read RX FIFO as we go
+          continue;
         }
+	  *(RxBuff++) = SPI0_FIFO;                                               // Read RX FIFO as we go
     }
-
-  SPI0_CONTROL &= ~BIT(SPI_C_TA);                                                   // Set TA = 0
+  SPI0_CONTROL &= ~BIT(SPI_C_TA);                                            // Set TA = 0
 }
 
-void spi_end(void)                                                                      // Set all the SPI0 pins back to input
+void spi_end(void)                                                           // Set all the SPI0 pins back to input
 {
-  gpioGPFSEL1 &=~((7 << 0) | (7 << 3));                                         // Set as input pins GPIO = 000
-  gpioGPFSEL0 &=~((7 << 21) | (7 << 24) | (7 << 27));                       // Set as input pins GPIO = 000                                                             // Write back updated value
+  gpioGPFSEL1 &=~((7 << 0) | (7 << 3));                                      // Set as input pins GPIO = 000
+  gpioGPFSEL0 &=~((7 << 21) | (7 << 24) | (7 << 27));                        // Set as input pins GPIO = 000                                                             // Write back updated value
 }
