de635de155e48c41d2bda4a9f3170868e7ba5d8f
David Banks <dave@hoglet.com>
2015-12-09 12:01:57 +0000

    Improved reporting of undefined instruction exceptions etc.
    Change-Id: Ic0be3f70056b7aecf6f87d2962dae0a0d7bcc579

--- bare-metal/tube-exception.c
+++ bare-metal/tube-exception.c
@@ -1,22 +1,60 @@
-#include <stdio.h>
 #include "rpi-aux.h"
 #include "rpi-gpio.h"
 
+void dump_hex(unsigned int h) {
+  int i;
+  for (i = 0; i < 8; i++) {   
+	int c = h >> 28;
+	if (c < 10) {
+	  c = '0' + c;
+	} else {
+	  c = 'A' + c - 10;
+	}
+	RPI_AuxMiniUartWrite(c);
+	h <<= 4;
+  }
+}
+
+// For some reason printf generally doesn't work here
 void dump_info(unsigned int *reg) {
   unsigned int *addr;
   int i;
+  dump_hex((unsigned int)reg);
+  RPI_AuxMiniUartWrite('\r');
+  RPI_AuxMiniUartWrite('\n');  
   for (i = 0; i <= 13; i++) {
-	printf("reg[%d] = %0x\r\n", i, reg[i]);
+	RPI_AuxMiniUartWrite('r');
+	RPI_AuxMiniUartWrite('e');  
+	RPI_AuxMiniUartWrite('g');  
+	RPI_AuxMiniUartWrite('[');  
+	RPI_AuxMiniUartWrite('0' + (i / 10));  
+	RPI_AuxMiniUartWrite('0' + (i % 10));  
+	RPI_AuxMiniUartWrite(']');
+	RPI_AuxMiniUartWrite('=');
+	dump_hex(reg[i]);
+	RPI_AuxMiniUartWrite('\r');
+	RPI_AuxMiniUartWrite('\n');  
   }
   addr = (unsigned int *)(reg[13] & ~3);
   for (i = -4; i <= 4; i++) {
-	printf("%08x = %08x\n", (unsigned int) (addr + i), *(addr + i)); 
+	dump_hex((unsigned int) (addr + i));
+	RPI_AuxMiniUartWrite('=');
+	dump_hex(*(addr + i));	
+	RPI_AuxMiniUartWrite('\r');
+	RPI_AuxMiniUartWrite('\n');  
   }
-  printf("Halted\r\n");
+  RPI_AuxMiniUartWrite('H');
+  RPI_AuxMiniUartWrite('a');
+  RPI_AuxMiniUartWrite('l');
+  RPI_AuxMiniUartWrite('t');
+  RPI_AuxMiniUartWrite('e');
+  RPI_AuxMiniUartWrite('d');
+  RPI_AuxMiniUartWrite('\r');
+  RPI_AuxMiniUartWrite('\n');  
   while (1) {
-	for (i = 0; i < 10000000; i++);
+	for (i = 0; i < 1000000; i++);
 	LED_ON();
-	for (i = 0; i < 10000000; i++);
+	for (i = 0; i < 1000000; i++);
 	LED_OFF();
   }
 
