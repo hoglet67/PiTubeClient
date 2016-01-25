c25ae749f79b5386cae02d3e0189e266d274efed
David Banks <dave@hoglet.com>
2016-01-25 16:28:30 +0000

    Fixed some compile warings in cpu80186.h
    Change-Id: I4320eb6f2760f0752165c68bd9681bc200837dc8

--- cpu80186/cpu80186.h
+++ cpu80186/cpu80186.h
@@ -62,10 +62,10 @@ union _bytewordregs_ {
 };
 
 #define StepIP(x)	ip += x
-#define getmem8(x, y)	read86(segbase(x) + y)
-#define getmem16(x, y)	readw86(segbase(x) + y)
-#define putmem8(x, y, z)	write86(segbase(x) + y, z)
-#define putmem16(x, y, z)	writew86(segbase(x) + y, z)
+#define getmem8(x, y)	read86(segbase(x) + (y))
+#define getmem16(x, y)	readw86(segbase(x) + (y))
+#define putmem8(x, y, z)	write86(segbase(x) + (y), (z))
+#define putmem16(x, y, z)	writew86(segbase(x) + (y), (z))
 #define signext(value)	(int16_t)(int8_t)(value)
 #define signext32(value)	(int32_t)(int16_t)(value)
 #define getreg16(regid)	regs.wordregs[regid]
@@ -74,7 +74,7 @@ union _bytewordregs_ {
 #define putreg8(regid, writeval)	regs.byteregs[byteregtable[regid]] = writeval
 #define getsegreg(regid)	segregs[regid]
 #define putsegreg(regid, writeval)	segregs[regid] = writeval
-#define segbase(x)	((uint32_t) x << 4)
+#define segbase(x)	((uint32_t) ((x) << 4))
 
 extern void reset(void);
 extern void exec86(uint32_t execloops);
