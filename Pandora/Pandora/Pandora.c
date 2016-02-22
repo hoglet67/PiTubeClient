3654cfcd6428dbbea22cad9629046b198204f879
David Banks <dave@hoglet.com>
2016-02-22 11:13:57 +0000

    Updated Pandora.c to build on Linux
    Change-Id: I49979c1c95272f4e5473649122db4bed759360c7

--- Pandora/Pandora/Pandora.c
+++ Pandora/Pandora/Pandora.c
@@ -1,5 +1,7 @@
 // Pandora.c
+#ifndef __linux
 #include "stdafx.h"
+#endif
 
 #include <stdint.h>
 #include <stdio.h>
@@ -35,7 +37,11 @@ void init(void)
 #endif
 }
 
+#ifdef __linux
+int main(int argc, char* argv[])
+#else
 int _tmain(int argc, _TCHAR* argv[])
+#endif
 {
 	init();
 	n32016_reset();
