ee959de61e424a7298e359a23ca674db8adfffa0
fordp2002 <SimonEllwood@hotmail.com>
2016-02-22 13:54:43 +0000

    32016 add some RAM constants

--- NS32016/32016.h
+++ NS32016/32016.h
@@ -1,7 +1,14 @@
-#define K640  0x00A0000
-#define MEG1  0x0100000
-#define MEG4  0x0400000
-#define MEG16 0x1000000
+// Some meaningful memory sizes
+#define K128      0x0020000
+#define K256      0x0040000
+#define K512      0x0080000
+#define K640      0x00A0000
+#define MEG1      0x0100000
+#define MEG2      0x0200000
+#define MEG4      0x0400000
+// I think the 32016 CoPro could support 16256K Bytes of RAM ;)
+#define MAX_RAM   (MEG16 - K128)
+#define MEG16     0x1000000
 
 #define MEM_MASK 0xFFFFFF
 #define RAM_SIZE 0x100000
