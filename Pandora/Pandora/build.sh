3ab3d3bcbcc906aa6d7d931c6cfa20c957b8ffa8
David Banks <dave@hoglet.com>
2016-03-06 11:03:43 +0000

    32016: Added small build linux script for test suite
    Change-Id: I3b37ef59cc02b6739d7a97d26665d5902208a19f

---
+++ Pandora/Pandora/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc -DTRACE_TO_CONSOLE -DSHOW_INSTRUCTIONS -DTEST_SUITE -o Pandora Pandora.c -I ../../NS32016/ ../../NS32016/32016.c ../../NS32016/NSDis.c  ../../NS32016/mem32016.c ../../NS32016/Trap.c
