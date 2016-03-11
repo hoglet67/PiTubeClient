179205ebc38b188dff48aa6d495b071ebbffa896
David Banks <dave@hoglet.com>
2016-03-11 15:19:24 +0000

    32016: Updated fpga tool to handle write addresses
    Change-Id: Ib2a347372ece635334b0f1c7cf97e5a2095b5847

--- tools/fpga_trace/process.c
+++ tools/fpga_trace/process.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// 0 = PC change
+// 1 = Write
+#define TYPE_BIT (1 << 29)
+
 int process(char *old_filename, char  *new_filename)
 {
    FILE  *ptr_old, *ptr_new;
@@ -9,6 +13,7 @@ int process(char *old_filename, char  *new_filename)
    int last;
    int count = 0;
    int address = -1;
+   int be;
 
    ptr_old = fopen(old_filename, "rb");
    if(ptr_old == 0)
@@ -37,12 +42,41 @@ int process(char *old_filename, char  *new_filename)
       // Look for a 0->1 transition on bit 7
       if (last >= 0 && !(last & 0x80) && (a & 0x80))
       {
+         // printf("%02x\n", a);
          // If bit 6 is 1, this is the start of a new character
          if (a & 0x40)
          {
+            //printf("%08x\n", address);
             if (address != -1)
             {
-               fprintf(ptr_new, "&%06X\n", address);
+               if (address & TYPE_BIT)
+               {
+                  // A write has occurred
+                  // Bits 27..24 are the BE values
+                  // Bits 23..0 are the address
+                  be = (address >> 24) & 15;
+                  if (be & 1)
+                  {
+                     fprintf(ptr_new, "WR &%06X\n", address & 0xFFFFFC);
+                  }
+                  if (be & 2)
+                  {
+                     fprintf(ptr_new, "WR &%06X\n", (address & 0xFFFFFC) + 1);
+                  }
+                  if (be & 4)
+                  {
+                     fprintf(ptr_new, "WR &%06X\n", (address & 0xFFFFFC) + 2);
+                  }
+                  if (be & 8)
+                  {
+                     fprintf(ptr_new, "WR &%06X\n", (address & 0xFFFFFC) + 3);
+                  }
+               }
+               else
+               {
+                  // PC has changed
+                  fprintf(ptr_new, "PC &%06X\n", address);
+               }
             }
             address = a & 0x3F;
          }
