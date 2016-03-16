6d71de9d5bfd85b66912d43117ba9bfdecfe50f3
David Banks <dave@hoglet.com>
2016-03-16 18:49:19 +0000

    32016: Updated linux test build script to compile cpu and fpu versions
    Change-Id: I5e4a945292fc104f5c40d620b0f14ab4490170c8

--- Pandora/Pandora/build.sh
+++ Pandora/Pandora/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-gcc -DPROFILING -DTRACE_TO_CONSOLE -DSHOW_INSTRUCTIONS -DTEST_SUITE -o Pandora Pandora.c -I ../../NS32016/ ../../NS32016/32016.c ../../NS32016/NSDis.c  ../../NS32016/mem32016.c ../../NS32016/Trap.c ../../NS32016/Profile.c
+for i in 0 1
+do
+gcc -DPC_SIMULATION -DPROFILING -DTRACE_TO_CONSOLE -DSHOW_INSTRUCTIONS -DTEST_SUITE=${i} -o Pandora${i} Pandora.c -I ../../NS32016/ ../../NS32016/32016.c ../../NS32016/Decode.c ../../NS32016/NSDis.c  ../../NS32016/mem32016.c ../../NS32016/Trap.c ../../NS32016/Profile.c -lm
+done
 
